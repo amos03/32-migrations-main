2275b008a092298ed4be4f3f9ea1dff4b0650fbd
amos03 <amossi@gmail.com>
2021-07-26 13:16:08 -0400

    ran migration and rollback on party_guests

--- db/migrate/20210726165140_party_guests.rb
+++ db/migrate/20210726165140_party_guests.rb
@@ -1,4 +1,15 @@
 class PartyGuests < ActiveRecord::Migration[5.2]
   def change
+    create_table :party_guests do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :dietary_restrictions
+      t.integer :salary
+      t.integer :number_kids
+      t.text :vulnerabilities
+      t.string :illnesses
+      t.string :medication
+      t.string :voting_preferences
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_163020) do
+ActiveRecord::Schema.define(version: 2021_07_26_165140) do
 
   create_table "parts", force: :cascade do |t|
     t.string "name"
     t.string "manufacturer"
     t.decimal "cost"
     t.integer "quantity"
+  end
+
+  create_table "party_guests", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "dietary_restrictions"
+    t.integer "salary"
+    t.integer "number_kids"
+    t.text "vulnerabilities"
+    t.string "illnesses"
+    t.string "medication"
+    t.string "voting_preferences"
   end
 
 end
